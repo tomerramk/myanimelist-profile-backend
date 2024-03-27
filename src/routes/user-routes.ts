d2c38b6c1e6757896c9991153a1d2b200750430d
Tomer Ramkovich <tommerramk@gmail.com>
2024-03-27 23:23:29 +0200

    feat: get all animelist entries route

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -19,6 +19,40 @@ router.get("/users/:username", async (req: Request, res: Response) => {
 });
 
 router.get(
+	"/users/animelist/all/:username",
+	async (req: Request, res: Response) => {
+		const username = req.params.username;
+
+		try {
+			const response = await axios.get(
+				`https://api.myanimelist.net/v2/users/${username}/animelist?fields=list_status&limit=1000`,
+				{
+					headers: { "X-MAL-CLIENT-ID": "14d56d1593e4ef012169f1c095749f1c" },
+				},
+			);
+
+			const animeCards = response.data.data.map((item) => ({
+				id: item.node.id,
+				title: item.node.title,
+				main_picture: item.node.main_picture,
+				status: item.list_status.status,
+				score: item.list_status.score,
+				num_episodes_watched: item.list_status.num_episodes_watched,
+				is_rewatching: item.list_status.is_rewatching,
+				updated_at: item.list_status.updated_at,
+				finish_date: item.list_status.finish_date,
+			}));
+
+			res.json({
+				data: animeCards,
+			});
+		} catch (error) {
+			res.status(500).json({ error: "Internal Server Error" });
+		}
+	},
+);
+
+router.get(
 	"/users/animelist/:username",
 	async (req: Request, res: Response) => {
 		const username = req.params.username;
